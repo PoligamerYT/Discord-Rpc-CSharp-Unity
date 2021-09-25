4ee09b54a61747439cdc48893a53efd0a2d95888
PoligamerYT <73081749+PoligamerYT@users.noreply.github.com>
2021-09-24 22:38:04 -0500

    Add files via upload

---
+++ Unity Version/Example/Example.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DiscordRichPresence;
+
+public class Example : MonoBehaviour
+{
+    private DiscordRpc.EventHandlers handlers;
+    private DiscordRpc.RichPresence presence;
+
+    void Start()
+    {
+        this.handlers = default(DiscordRpc.EventHandlers);
+        DiscordRpc.Initialize("Bot ID", ref this.handlers, true, null);
+        this.handlers = default(DiscordRpc.EventHandlers);
+        DiscordRpc.Initialize("Bot ID", ref this.handlers, true, null);
+        this.presence.details = "Text 1";
+        this.presence.state = "Text 2";
+        this.presence.largeImageKey = "Image 1";
+        this.presence.smallImageKey = "Image 2";
+        this.presence.largeImageText = "Image 1 Text";
+        this.presence.smallImageText = "Image 2 Text";
+        DiscordRpc.UpdatePresence(ref this.presence);
+    }
+}
---
+++ Unity Version/Example/ExampleDiscordShutdown.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DiscordRichPresence;
+
+public class ExampleDiscordShutdown : MonoBehaviour
+{
+    void OnApplicationQuit()
+    {
+        DiscordRpc.Shutdown();
+    }
+}
