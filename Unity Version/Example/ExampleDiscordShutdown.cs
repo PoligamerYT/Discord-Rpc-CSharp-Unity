7eadc295bbae48e8cb332c553dc5d76ff3239d04
PoligamerYT <73081749+PoligamerYT@users.noreply.github.com>
2021-09-24 19:20:37 -0500

    Add files via upload

---
+++ Unity Version/Example/ExampleDiscordShutdown.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DiscordRichPresence;
+
+public class ExampleDiscordShutDown : MonoBehaviour
+{
+    void OnApplicationQuit()
+    {
+        DiscordRpc.Shutdown();
+    }
+}
